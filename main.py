8d6ac0f2b491e3d186b3efe53785cddabde60823
Galactius <dperez6@villanova.edu>
2022-12-12 11:41:00 +0100

    Revised comments around program, next feature (implementing the duration of one's stay on the label) has been partially implemented.

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-# python-docx, package installed with venv, enables R/W access for Word Docs.
-from docx import Document
+# Python script developed by Daniel Perez, Villanova Vatican Internship Program 2022 - Assignment: Augustinians
+import sys
+import os
+from docx import Document                    # python-docx, enables R/W access for Word Docs.
 from docx.shared import Pt
 from docx.enum.style import WD_STYLE_TYPE
-from docx2pdf import convert
-from datetime import date
-import os
-
-# tkinter imports
-import tkinter as tk
+from docx2pdf import convert                # docx2pdf, enables conversion between docx and pdf
+from datetime import date                   # datetime, used to make use of dates
+import tkinter as tk                        # tkinter, Python GUI framework
 from tkinter import simpledialog
 from tkinter import messagebox
 
 
 class MainApplication(tk.Frame):
-    def validate_name_entry(input):
-        return isinstance(input, str)
-    def validate_room_entry(input):
-        return isinstance(input, int)
     def __init__(self, parent, *args, **kwargs):
         # Setup inheritance for tkinter and remove initialized window
         tk.Frame.__init__(self, parent, *args, **kwargs)
@@ -24,41 +19,42 @@ class MainApplication(tk.Frame):
         self.parent.title('My Window')
         root.withdraw()
 
-        # TODO: Add ability to add multiple labels to the same page
-        # TODO: ADD ability to enter date/time for reservation
-
         # Get current date/time
         date_str = date.today()
 
-        # Setup Document and Paragraph for labels
+        # Setup Word Document and Paragraph for labels
         document = Document()
         label = document.add_paragraph()
 
         labels_list = []
 
         # Setup styling for thicker label
-        # TODO add styling for smaller date on label
         font_styles = document.styles
         font_charstyle = font_styles.add_style('thick_label', WD_STYLE_TYPE.CHARACTER)
         font_object = font_charstyle.font
-        font_object.size = Pt(20)
+        font_object.size = Pt(25)
         font_object.bold = True
         font_object.name = 'Times New Roman'
 
-        # Styling for thinner label
-        # font_styles2 = document.styles
-        # font_charstyle2 = font_styles2.add_style('thin_label', WD_STYLE_TYPE.CHARACTER)
-        # font_object2 = font_charstyle2.font
-        # font_object2.size = Pt(20)
-        # font_object2.bold = False
-        # font_object2.name = 'Times New Roman'
+        # Setup styling for date label
+        font_styles2 = document.styles
+        font_charstyle2 = font_styles2.add_style('date', WD_STYLE_TYPE.CHARACTER)
+        font_object2 = font_charstyle2.font
+        font_object2.size = Pt(10)
+        font_object2.bold = False
+        font_object2.name = 'Times New Roman'
 
         for x in range(3):
             # Create Dialog Prompts
-            # TODO: Implement input validation
-            dlg_name = simpledialog.askstring('name', 'Enter Guest Name: (Press enter to skip)', parent=parent)
-            dlg_room_no = simpledialog.askinteger('room', 'Enter Guest Room Number:(Enter 0 to skip)', parent=parent)
+            dlg_name = simpledialog.askstring('Name Label ' + str(x + 1) + ' of 3', 'Enter Guest Name: (Press enter to skip)', parent=parent)
+            dlg_room_no = simpledialog.askinteger('Room No. Label ' + str(x + 1) + ' of 3', 'Enter Guest Room Number: (Enter 0 to skip)', parent=parent)
+            # dlg_date = simpledialog.askstring('Date Label ' + str(x + 1) + ' of 3', 'Enter dates for stay MM/DD/ - MM/DD: (Press Enter to skip)', parent=parent)
+
+            if(dlg_name is None or dlg_room_no is None):
+                sys.exit("Operation Cancelled (user pressed cancel)")
+
             string = dlg_name + "  " + str(dlg_room_no)
+            #dur_str = str(dlg_date)
             labels_list.append(string)
 
             # Create confirmation message and end tkinter root
@@ -76,15 +72,19 @@ class MainApplication(tk.Frame):
 
             # Add text to docx and save
             label.add_run(cur_str, style='thick_label')
+            # label.add_run(dur_str, style='date')
             label.add_run("\n")
             label.add_run("\n")
+            label.add_run("\n")
+            label.add_run("\n")
+        # Name new label document and save to file
         cur_document = "labels - " + str(date_str) + ".docx"
         document.save(cur_document)
 
         # Covert document to pdf for printing (to physical printer) then delete printed pdf.
         # needed to resolve XPS error when printing .docx files
         convert(cur_document, "labels.pdf")
-        # os.system("lp labels.pdf")
+        os.system("lp labels.pdf")
         os.remove("labels.pdf")
 
 
